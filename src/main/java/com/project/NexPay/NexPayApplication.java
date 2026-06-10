8fcdd79de45ec7bc74df723f570ed8fc225c670c
Naman Khurana <namankhurana2017@gmail.com>
2026-06-10 20:24:51 +0530

    init commit

---
+++ src/main/java/com/project/NexPay/NexPayApplication.java
@@ -0,0 +1,13 @@
+package com.project.NexPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexPayApplication.class, args);
+	}
+
+}
